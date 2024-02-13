50dae5c6c545a1d5a26bbdb52adaede8acd03c8b
Keana Cobarde <keanacobarde@gmail.com>
2024-02-12 21:26:32 -0600

    Add project files.

---
+++ Models/UserProfile.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CreekRiver.Models;
+
+public class UserProfile
+{
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string Email { get; set; }
+
+    public List<Reservation> Reservations { get; set; }
+
+}
